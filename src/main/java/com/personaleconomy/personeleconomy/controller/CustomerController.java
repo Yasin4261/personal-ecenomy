46d8b2aaf698f2df80f0e8201edfb29885e06933
yasin.gunes <yasin01ysn@gmail.com>
2024-01-03 16:41:54 +0300

    Added controller for deployment test

---
+++ src/main/java/com/personaleconomy/personeleconomy/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.personaleconomy.personeleconomy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerController {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
